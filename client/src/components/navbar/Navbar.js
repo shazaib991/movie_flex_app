b31c82b3c849f684325cbfcfb5451b92673fd6f1
shazaib munawar <shazaib424647@gmail.com>
2022-08-16 21:40:24 +0500

    added navbar

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar-container">
+      <div className="navbar"></div>
+    </nav>
+  );
+}
+
+export default Navbar;
