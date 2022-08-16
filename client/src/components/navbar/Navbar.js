76d88b91aafc24f032829644e882a873ffdfa3fc
shazaib munawar <shazaib424647@gmail.com>
2022-08-16 22:00:39 +0500

    added navbar links

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -6,6 +6,17 @@ function Navbar() {
     <nav className="navbar-container">
       <div className="navbar">
         <div className="navbar-logo">movie flex</div>
+        <ul className="navbar-links">
+          <li className="navbar-link">
+            <a href="/">movies</a>
+          </li>
+          <li className="navbar-link">
+            <a href="/">about us</a>
+          </li>
+          <li className="navbar-link">
+            <a href="/">contact us</a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
