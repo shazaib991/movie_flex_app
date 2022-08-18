00eda870ef5acdd2dbb76ee6f227ef0cd3ef4f64
shazaib munawar <shazaib424647@gmail.com>
2022-08-18 19:08:52 +0500

    created pagination and styled movie table

--- client/src/components/movieSection/MovieSection.js
+++ client/src/components/movieSection/MovieSection.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./movieSection.css";
+import editIcon from "../../icons/icons8-edit-384.png";
+import deleteIcon from "../../icons/icons8-delete-144.png";
 
 function MovieSection() {
   return (
@@ -33,6 +35,48 @@ function MovieSection() {
               <option value="movieName">movie name</option>
               <option value="movieRating">movie rating</option>
             </select>
+          </div>
+          <div className="movie-list-table">
+            <table>
+              <thead>
+                <tr>
+                  <th>movie name</th>
+                  <th>movie rating</th>
+                  <th>user name</th>
+                  <th>actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>movie 1</td>
+                  <td>4</td>
+                  <td>user 1</td>
+                  <td>
+                    <img src={editIcon} alt="edit" />
+                    <img src={deleteIcon} alt="edit" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>movie 2</td>
+                  <td>5</td>
+                  <td>user 2</td>
+                  <td>
+                    <img src={editIcon} alt="edit" />
+                    <img src={deleteIcon} alt="edit" />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="movie-pagination">
+            <div className="movie-pagination-navigators-container">
+              <div className="movie-pagination-navigators">1</div>
+              <div className="movie-pagination-navigators">2</div>
+              <div className="movie-pagination-navigators">3</div>
+              <div className="movie-pagination-navigators">4</div>
+              <div className="movie-pagination-navigators">5</div>
+            </div>
+            <button>Download PDF</button>
           </div>
         </div>
       </div>
