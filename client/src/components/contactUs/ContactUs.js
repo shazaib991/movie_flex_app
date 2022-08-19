c112cd744bb79325e304f3e944a4c68b75d7e269
shazaib munawar <shazaib424647@gmail.com>
2022-08-19 21:47:51 +0500

    created contact us

--- client/src/components/contactUs/ContactUs.js
+++ client/src/components/contactUs/ContactUs.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 function ContactUs() {
-  return <div>ContactUs</div>;
+  return (
+    <div className="contact-us-container">
+      <h1>contact us</h1>
+      <div className="contact-us">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          earum excepturi accusamus cupiditate! Pariatur sint molestiae ab
+          tempora a aliquid expedita, labore consectetur eum voluptates, quos
+          qui dignissimos fugiat quae.
+        </p>
+        <div className="contact-us-form">
+          <input type="text" placeholder="enter your name" />
+          <input type="email" placeholder="enter your email" />
+          <input type="text" placeholder="about yourself" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactUs;
