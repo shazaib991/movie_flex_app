83e532de6e71b112920bb95c306e5207db9ae5be
shazaib munawar <shazaib424647@gmail.com>
2022-08-19 21:33:33 +0500

    added and styled aboutus

--- client/src/components/aboutUs/AboutUs.js
+++ client/src/components/aboutUs/AboutUs.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./aboutUs.css";
 
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="aboutUs-container">
+      <h1>about us</h1>
+      <div className="aboutUs">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed hic ad
+          accusantium minus pariatur ipsum ex modi maiores error. Molestias,
+          cupiditate veritatis? Quis obcaecati nam qui odio optio ad, similique
+          enim odit perspiciatis reprehenderit eveniet. Repellendus porro
+          asperiores, assumenda voluptate culpa nostrum cupiditate quidem
+          adipisci omnis sapiente, dolores, blanditiis necessitatibus.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
