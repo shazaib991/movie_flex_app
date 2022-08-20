1cdea4a06dca08d4f0d3288f3e377b779834b0cd
shazaib munawar <shazaib424647@gmail.com>
2022-08-20 20:11:04 +0500

    styled contact section

--- client/src/components/contactUs/ContactUs.js
+++ client/src/components/contactUs/ContactUs.js
@@ -1,20 +1,23 @@
 import React from "react";
+import "./contactUs.css";
 
 function ContactUs() {
   return (
     <div className="contact-us-container">
       <h1>contact us</h1>
-      <div className="contact-us">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          earum excepturi accusamus cupiditate! Pariatur sint molestiae ab
-          tempora a aliquid expedita, labore consectetur eum voluptates, quos
-          qui dignissimos fugiat quae.
-        </p>
-        <div className="contact-us-form">
-          <input type="text" placeholder="enter your name" />
-          <input type="email" placeholder="enter your email" />
-          <input type="text" placeholder="about yourself" />
+      <div className="contact-us-wrapper">
+        <div className="contact-us">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            earum excepturi accusamus cupiditate! Pariatur sint molestiae ab
+            tempora a aliquid expedita, labore consectetur eum voluptates, quos
+            qui dignissimos fugiat quae.
+          </p>
+          <div className="contact-us-form">
+            <input type="text" placeholder="enter your name" />
+            <input type="email" placeholder="enter your email" />
+            <input type="text" placeholder="about yourself" />
+          </div>
         </div>
       </div>
     </div>
